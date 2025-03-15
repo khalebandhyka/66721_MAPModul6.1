b2789d48f00895e583ed1bef1f5a254b6b0f9181
Khaleb Andhyka Aprijadi <khaleb.andhyka@gmail.com>
2025-03-15 13:05:45 +0700

    Add the save Functionality

--- app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.inventory.ui.item
 
 import androidx.compose.foundation.layout.Arrangement
@@ -47,6 +31,8 @@ import com.example.inventory.ui.navigation.NavigationDestination
 import com.example.inventory.ui.theme.InventoryTheme
 import java.util.Currency
 import java.util.Locale
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
 
 object ItemEntryDestination : NavigationDestination {
     override val route = "item_entry"
@@ -88,22 +74,29 @@ fun ItemEntryScreen(
 
 @Composable
 fun ItemEntryBody(
+
     itemUiState: ItemUiState,
     onItemValueChange: (ItemDetails) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val coroutineScope = rememberCoroutineScope()
+
     Column(
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
-        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
-        ) {
+        modifier = modifier.then(Modifier.padding(dimensionResource(id = R.dimen.padding_medium)))
+    ) {
         ItemInputForm(
             itemDetails = itemUiState.itemDetails,
             onValueChange = onItemValueChange,
             modifier = Modifier.fillMaxWidth()
         )
         Button(
-            onClick = onSaveClick,
+            onClick = {
+                coroutineScope.launch {
+                    onSaveClick()
+                }
+            },
             enabled = itemUiState.isEntryValid,
             shape = MaterialTheme.shapes.small,
             modifier = Modifier.fillMaxWidth()
@@ -112,6 +105,7 @@ fun ItemEntryBody(
         }
     }
 }
+
 
 @Composable
 fun ItemInputForm(
